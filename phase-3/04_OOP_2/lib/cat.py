a97ea6f1896d8c5ab5316c7c8d0ad37e036afdaa
Anthony Beltran <beltrananthony30@icloud.com>
2023-09-18 14:41:47 -0700

    "complete"

--- phase-3/04_OOP_2/lib/cat.py
+++ phase-3/04_OOP_2/lib/cat.py
@@ -1,6 +1,45 @@
 # 7. ✅. Create a subclass of Pet called Cat
-
 # import Pet from lib.pet
+from lib.pet import Pet
+
+
+class Cat(Pet):
+    all = []
+
+    def __init__(self, name, age, breed, temperament, indoor, owner=None):
+        super().__init__(name, age, breed, temperament)
+        self.indoor = indoor
+        self.__class__.all.append(self)
+        self.owner = owner
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        from lib.owner import Owner
+
+        if owner is None or isinstance(owner, Owner):
+            self._owner = owner
+
+    @classmethod
+    def all_cats(cls):
+        return [cat for cat in cls.all if isinstance(cat, cls)]
+
+
+# class Dog(Pet):
+#     def __init__(self, name, age, breed, temperament):
+#         self.name = name
+#         self.age = age
+#         self.breed = breed
+#         self.temperament = temperament
+#         self.__class__.add_to_all(self)
+
+
+# cookie = Cat("cookie", 1, "Dachshund", "hyper", True)
+
+# dog = Dog("cookie", 1, "Dachshund", "hyper")
 
 # 8. ✅. Create Cat class + __init__ that takes all the parameters from Pet and an
 # additional parameter called indoor (Boolean)
