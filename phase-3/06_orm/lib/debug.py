907b03c0bdaad71b730744bffeec1bedf2106f1e
Anthony Beltran <beltrananthony30@icloud.com>
2023-10-03 11:09:06 -0700

    "updated"

--- phase-3/06_orm/lib/debug.py
+++ phase-3/06_orm/lib/debug.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
+import ipdb
+from pet import Pet
 
 # Owner.create_table()
 # frank = Owner("frank", "555-555-5555", "frank@gmail.com", "555 Somewhere St.")
 # frank.save()
 
-# Pet.create_table()
+Pet.drop_table()
+Pet.create_table()
 # spot = Pet("spot", "dog", "chihuahua", "feisty")
-# spot.save()
 
-import ipdb
+# spot.save()
+spot = Pet("spot", "dog", "chihuahua", "feisty")
+snowball = Pet("snowball", "dog", "Poodle", "friendly")
 
 ipdb.set_trace()
